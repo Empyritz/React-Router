9cf7caf71b99da2fe837d624d6ccff7cb1cc3fe2
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-01 16:08:33 -0500

    Using nest routes to manage the paths and sahring layouts to them, also created a protectedRoute to be show if user exist else the path navigate to home or login again to finish the form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
@@ -5,8 +6,15 @@ import Products from "./pages/Products"
 import Error from "./pages/Error"
 import SharedLayout from "./pages/SharedLayout"
 import SingleProduct from "./pages/SingleProduct"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./pages/ProtectedRoute"
+import SharedProductLayout from "./pages/SharedProductLayout"
+
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <BrowserRouter>
       {/* <nav>our navbar</nav> */}
@@ -14,8 +22,16 @@ function App() {
           <Route path="/" element={<SharedLayout />} > 
             <Route index element={<Home />}  />
             <Route path="about" element={<About />}  />
-            <Route path="products" element={<Products />}  />
-            <Route path="products/:productId" element={<SingleProduct />}  />
+            <Route path="products" element={<SharedProductLayout/>}>
+              <Route index element={<Products />}  />
+              <Route path=":productId" element={<SingleProduct />}  />
+            </Route>
+            <Route path="login" element={<Login setUser={setUser} />} />
+            <Route path="dashboard" element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user}/>
+              </ProtectedRoute>
+            } />
             <Route path="*" element={<Error />}  />
           </Route>
         </Routes>
--- my-app/src/pages/Products.js
+++ my-app/src/pages/Products.js
@@ -14,7 +14,6 @@ const Products = () => {
   return (
     <>
       <section className='section'>
-        <h2>products</h2>
         <div className="products">
           {listProducts}
         </div>
